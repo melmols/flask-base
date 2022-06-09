fd7f7c044d33e19d127250bdc149206550662e3f
Melanie Molina Sandoval <melanie@Melanies-MacBook-Pro.local>
2022-06-09 09:36:33 +0100

    changed start port

--- cinema/app/app.py
+++ cinema/app/app.py
@@ -1,12 +1,4 @@
 from flask import Flask, redirect, render_template, make_response, request
-from datetime import datetime
-
-import uuid
-import pickle
-import base64
-
-import sys
-
 app = Flask(__name__)
 
 
@@ -16,4 +8,4 @@ def root():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=800)
+    app.run(host="0.0.0.0")
